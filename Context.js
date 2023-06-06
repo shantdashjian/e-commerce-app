7d7d8b55ae2073ac4c8fb20def9f379a7bac0668
shantdashjian <shantgdashjian@gmail.com>
2023-06-06 19:21:27 +0400

    Fetch the all photos data

--- Context.js
+++ Context.js
@@ -3,8 +3,13 @@ import React from 'react'
 const Context = React.createContext()
 
 function ContextProvider(props) {
-	const [allPhotos, setAllPhotos] =React.useState([])
+	const [allPhotos, setAllPhotos] = React.useState([])
 
+	React.useEffect(() => {
+		fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+			.then(res => res.json())
+			.then(data => setAllPhotos(data))
+	}, [])
 	return (
 		<Context.Provider value={{allPhotos}}>
 			{props.children}
