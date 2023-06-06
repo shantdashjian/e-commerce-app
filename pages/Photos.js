da88cd586634748a561696a9bdb3103830900683
shantdashjian <shantgdashjian@gmail.com>
2023-06-06 19:43:54 +0400

    Render all images using data from context

--- pages/Photos.js
+++ pages/Photos.js
@@ -1,9 +1,18 @@
-import React from "react"
+import React, {useContext} from "react"
+import {Context} from "../Context";
+import Image from "../components/Image";
+import {getClass} from "../utils";
 
 function Photos() {
+	const {allPhotos} = useContext(Context)
+	const images = allPhotos
+			.map((photo, index) =>
+				<Image key={photo.id} img={photo} className={getClass(index)} />
+			)
+
 	return (
 		<main className="photos">
-			<h1>Images go here</h1>
+			{images}
 		</main>
 	)
 }
