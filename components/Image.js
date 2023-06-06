d76444fb6382eaf0aad8773973eec9cee01cc20a
shantdashjian <shantgdashjian@gmail.com>
2023-06-06 21:33:57 +0400

    Add icons to the images

--- components/Image.js
+++ components/Image.js
@@ -1,14 +1,21 @@
 import React from "react"
 
 function Image({className, img}) {
-	const [ hovered, setHovered ] = React.useState(false)
+	const [hovered, setHovered] = React.useState(false)
 
 	function toggleHovered() {
 		setHovered(prev => !prev)
 	}
 
+	const icons = hovered ? (
+		<React.Fragment>
+			<i className="ri-heart-line favorite"></i>
+			<i className="ri-add-circle-line cart"></i>
+		</React.Fragment>
+	) : null
 	return (
 		<div className={`${className} image-container`} onMouseEnter={toggleHovered} onMouseLeave={toggleHovered}>
+			{icons}
 			<img src={img.url} className="image-grid"/>
 		</div>
 	)
