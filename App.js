a1df5ba52cfc2b0338955517273569edc05ac9b4
shantdashjian <shantgdashjian@gmail.com>
2023-06-06 17:23:37 +0400

    Add routing

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import React from "react"
 import Header from "./components/Header"
+import {Route, Switch} from "react-router-dom";
+import Photos from "./pages/Photos";
+import Cart from "./pages/Cart";
 
 function App() {
 	return (
 		<div>
 			<Header/>
-			<h1>Home Page</h1>
+			<Switch>
+				<Route exact path="/">
+					<Photos />
+				</Route>
+				<Route path="/cart">
+					<Cart />
+				</Route>
+			</Switch>
 		</div>
 	)
 }
