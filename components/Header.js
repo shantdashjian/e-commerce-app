b4c3fc41b7ec6084c0806e3c97d5dcd6ca5686da
shantdashjian <shantgdashjian@gmail.com>
2023-06-08 04:35:58 +0400

    Show full cart icon when there are items in cart

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,16 @@
 import React from "react"
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+
+import {Context} from "../Context";
 
 function Header() {
+	const {cartItems} = React.useContext(Context)
+	const iconClassName = cartItems.length > 0 ? "ri-shopping-cart-fill" : "ri-shopping-cart-line"
+
 	return (
 		<header>
 			<Link to="/"><h2>Pic Some</h2></Link>
-			<Link to="/cart"><i className="ri-shopping-cart-line ri-fw ri-2x"></i></Link>
+			<Link to="/cart"><i className={`${iconClassName} ri-fw ri-2x`}></i></Link>
 		</header>
 	)
 }
